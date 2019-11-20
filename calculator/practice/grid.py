95c9c65bfda650a6339e9635c84382b440eb6f2a
upsicleclown <55859815+upsicleclown@users.noreply.github.com>
2019-11-19 23:25:58 -0500

    Create grid.py

---
+++ calculator/practice/grid.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+root = Tk()
+
+myLabel1 = Label(root, text = "Hello World")
+myLabel2 = Label(root, text = "names roxie")
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=0, column=2)
+
+
+root.mainloop()
+
+
+
+
